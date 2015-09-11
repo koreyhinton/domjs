50af1ac2e3dce89c815ab004282b57452ffa8b68
Korey Hinton <KoreyHinton@ITs-Mac-mini.local>
2015-09-11 15:23:54 -0600

    update

--- dom.js
+++ dom.js
@@ -1,87 +1,154 @@
-"use strict";
+// The MIT License (MIT)
 
-function dom(id) {
+// Copyright (c) 2015 Korey Hinton
 
-    var domObject;
-    var traverse = undefined;
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 
-    function expand(expanded) {
+/// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 
-        expanded.replace = function (html) {
-            expanded.innerHTML = html;
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+var dom = (function () {
+    'use strict';
+
+    var tags = ["html", "title", "body", "h1", "h2", "h3", "h4", "h5", "h6", "p", "br", "hr", "abbr", "address", "b", "i", "bdi", "bdo", "blockquote", "cite", "code", "del", "dfn", "em", "ins", "kbd", "mark", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "small", "strong", "sub", "sup", "time", "u", "var", "wbr", "form", "input", "textarea", "button", "select", "optgroup", "option", "label", "fieldset", "legend", "datalist", "keygen", "output", "iframe", "img", "map", "area", "canvas", "figcaption", "figure", "audio", "source", "track", "video", "a", "link", "nav", "ul", "ol", "li", "dl", "dt", "dd", "menu", "menuitem", "table", "caption", "th", "tr", "td", "thead", "tbody", "tfoot", "col", "colgroup", "style", "div", "span", "header", "footer", "main", "section", "article", "aside", "details", "dialog", "summary", "head", "meta", "base", "script", "noscript", "embed", "param"];
+
+    function dom() {
+
+        var domObject;
+        var traverse = undefined;
+
+        function expand(expanded) {
+
+            expanded.replace = function (html) {
+                expanded.innerHTML = html;
+                return expanded;
+            };
+
+            expanded.prepend = function (html) {
+                expanded.innerHTML = expanded.innerHTML + html;
+                return expanded;
+            };
+
+            expanded.append = function (html) {
+                expanded.innerHTML += html;
+                return expanded;
+            };
+
+            expanded.display = function (type) {
+                expanded.style.display = type;
+                return expanded;
+            };
+
+            expanded.visibility = function (visibility) {
+                expanded.style.visibility = visibility;
+                return expanded;
+            };
+
+            expanded.addClass = function (className) {
+                expanded.classList.add(className);
+                return expanded;
+            };
+
+            expanded.removeClass = function (className) {
+                expanded.classList.remove(className);
+                return expanded;
+            };
+
+            expanded.toggleClass = function (className) {
+                expanded.classList.toggle(className);
+                return expanded;
+            };
+
+            expanded.traverse = function (callback) {
+                traverse(expanded, callback);
+                return expanded;
+            };
+
+            // Functions with primitive return values
+            expanded.hasClass = function (className) {
+                return expanded.classList.contains(className);
+            };
+
             return expanded;
+        }
+
+
+        traverse = function (node, callback) {
+
+            var children = Array.prototype.slice.call(node.children);
+
+            children.forEach(function (value) {
+                var child = value;
+                callback(expand(child));
+                traverse(child, callback);
+            });
         };
 
-        expanded.prepend = function (html) {
-            expanded.innerHTML = expanded.innerHTML + html;
-            return expanded;
-        };
+        if (arguments.length === 0) {
 
-        expanded.append = function (html) {
-            expanded.innerHTML += html;
-            return expanded;
-        };
+            // body
 
-        expanded.display = function (type) {
-            expanded.style.display = type;
-            return expanded;
-        };
+            domObject = window.document.body;
+            expand(domObject);
+            
+        } else if (arguments[0].charAt(0) === '.' || tags.indexOf(arguments[0]) !== -1) {
 
-        expanded.visibility = function (visibility) {
-            expanded.style.visibility = visibility;
-            return expanded;
-        };
+            // tags by class name or by tag name
 
-        expanded.addClass = function (className) {
-            expanded.classList.add(className);
-            return expanded;
-        };
+            var elements = window.document.querySelectorAll(arguments[0]);
+            var domCollection = Array.prototype.slice.call(elements);
 
-        expanded.removeClass = function (className) {
-            expanded.classList.remove(className);
-            return expanded;
-        };
+            domCollection.forEach(function (value) {
+                expand(value);
+            });
 
-        expanded.toggleClass = function (className) {
-            expanded.classList.toggle(className);
-            return expanded;
-        };
+            domCollection.traverse = function (callback) {
 
-        expanded.traverse = function (callback) {
-            traverse(expanded, callback);
-            return expanded;
-        };
+                domCollection.forEach(function (node) {
+                    callback(node);
+                    node.traverse(callback);
+                });
+            };
 
-        // Functions with primitive return values
-        expanded.hasClass = function (className) {
-            return expanded.classList.contains(className);
-        };
+            domObject = domCollection;
 
-        return expanded;
+        } else if (dom.cache !== undefined) {
+
+            
+            if (dom.cache(arguments[0]) !== undefined) {
+
+                // single cached tag by id
+                domObject = dom.cache(arguments[0]);
+
+            } else {
+                domObject = dom.cache.add(arguments[0],expand(window.document.getElementById(arguments[0])));
+            }
+            
+        } else {
+
+            // single tag by id
+
+            var id = arguments[0];
+            domObject = window.document.getElementById(id);
+
+            expand(domObject);
+        }
+
+        return domObject;
     }
 
-
-    traverse = function (node, callback) {
-
-        var children = Array.prototype.slice.call(node.children);
-
-        children.forEach(function (value) {
-            var child = value;
-            callback(expand(child));
-            traverse(child, callback);
-        });
-    };
-
-    if (dom.cache !== undefined && dom.cache(id) !== undefined) {
-
-        domObject = dom.cache(id);
-
-    } else {
-
-        domObject = window.document.getElementById(id);
-
-        expand(domObject);
-    }
-
-    return domObject;
-}
+    return dom;
+}());
